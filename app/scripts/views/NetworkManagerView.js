1bc5be280b7bd4336c4b0e059342df856b0a3c9d
keiono <keiono@gmail.com>
2013-10-29 12:55:20 -0700

    Selected tree name will be rendered in orange.

--- app/scripts/views/NetworkManagerView.js
+++ app/scripts/views/NetworkManagerView.js
@@ -24,6 +24,8 @@ define([
             'click .popover-content .networkName': 'networkSelected'
         },
 
+        current: 'NeXO',
+
 
         initialize: function () {
             console.log(this.collection);
@@ -36,21 +38,35 @@ define([
 
             var listString = '<ul class="nav nav-pills nav-stacked">';
             var treeCount = this.collection.length;
+
             for (var i = 0; i < treeCount; i++) {
                 var network = this.collection.at(i);
                 var networkName = network.get('config').name;
                 console.log(networkName);
-                listString += '<li class="networkName">' + networkName + '</li>';
+                if(networkName === this.current) {
+                    listString += '<li class="networkName current">' + networkName + '</li>';
+                } else {
+                    listString += '<li class="networkName">' + networkName + '</li>';
+                }
             }
 
             listString += '</ul>';
             trees.attr('data-content', listString);
         },
 
+        /**
+         * Update flag to highlight selected tree.
+         *
+         * @param e
+         */
         networkSelected: function (e) {
             var selectedNetworkName = e.currentTarget.textContent;
             var selectedNetwork = this.collection.where({name: selectedNetworkName});
             this.collection.trigger(EventHelper.NETWORK_SELECTED, selectedNetwork);
+            $('.networkName:contains(\'' + selectedNetworkName + '\')').addClass('current');
+            this.current = selectedNetworkName;
+
+            this.render();
 
             // Hide popover
             this.$el.find('#trees').popover('hide');
