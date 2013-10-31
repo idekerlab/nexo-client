dbe7189bd02de59d95242e3ec60c43a358d278ff
keiono <keiono@gmail.com>
2013-10-30 17:44:27 -0700

    Children are colored in light green.

--- app/scripts/views/NetworkView.js
+++ app/scripts/views/NetworkView.js
@@ -17,10 +17,16 @@ define([
     'views/SigmaRenderer'
 ], function (jquery, _, Backbone, EventHelper, SigmaRenderer) {
 
-    // Constants
-    var DIM_COLOR = 'rgba(150,150,150,0.7)';
-    var SELECTED_NODE_COLOR = 'rgb(70,130,180)';
+    // Constants: Preset colors for selection
+    var DIM_COLOR = 'rgba(150,150,150,0.6)';
+    var SELECTED_NODE_COLOR = 'rgb(22,149,163)';
     var QUERY_NODE_COLOR = 'rgb(255,94,25)';
+    var CHILD_NODE_COLOR = 'rgb(190,219,57)';
+
+    var EXTRA_EDGE_COLOR = 'rgba(235,127,0,0.3)';
+
+    // Special edge types
+    var EXTRA_EDGE = 'extra';
 
     var LOADING_ICON = '<div id="facebookG">' +
         '<div id="blockG_1" class="facebook_blockG"></div>' +
@@ -122,7 +128,7 @@ define([
             // Do nothing if ID is invalid.
             if (id === undefined || id === '' || id === null) {
                 return;
-            } else if(this.model.get('convert')[id] !== undefined) {
+            } else if (this.model.get('convert')[id] !== undefined) {
                 id = this.model.get('convert')[id];
             }
 
@@ -228,18 +234,23 @@ define([
         addHiddenEdges: function (pathArray) {
 
             // Query node is ALWAYS idx 0 of list.
-            var source = pathArray[0][0];
-            var targets = [];
+            var target = pathArray[0][0];
+            // ...and this is always its parent term in the tree.
+            var parentTerm = pathArray[0][1];
+
+            var children = [];
 
             _.each(pathArray, function (path) {
-                targets.push(path[1]);
+                if (path[0] !== parentTerm) {
+                    children.push(path[0]);
+                }
             });
 
             // Reset current state
             var edgeNames = {};
             SigmaRenderer
                 .iterEdges(function (edge) {
-                    if (edge.label === 'extra') {
+                    if (edge.label === EXTRA_EDGE) {
                         SigmaRenderer.dropEdge(edge.id);
                     } else {
                         var edgeName = edge.source + '-' + edge.target;
@@ -247,12 +258,12 @@ define([
                     }
                 });
 
+            // Add hidden edges
             var extraEdges = [];
-
-            _.each(targets, function (target) {
+            _.each(children, function (source) {
                 var edgeName = source + '-' + target;
                 if (!edgeNames[edgeName]) {
-                    // Edge does not exists.  Add to view
+                    // Edge does not exists (hidden).  Add to view
                     if (SigmaRenderer._core.graph.nodesIndex[source] &&
                         SigmaRenderer._core.graph.nodesIndex[target]) {
 
@@ -262,11 +273,11 @@ define([
                             target: target,
                             weight: 0.8,
                             size: 0.8,
-                            label: 'extra',
+                            label: EXTRA_EDGE,
                             id: edgeName,
                             type: 'curve',
                             attr: {
-                                type: 'extra'
+                                type: EXTRA_EDGE
                             }
                         };
                         if (!SigmaRenderer._core.graph.edgesIndex[edgeName]) {
@@ -286,24 +297,31 @@ define([
          * @param pathArray - Array of lists.  Each list contains sequence of Term IDs.
          */
         showPath: function (pathArray) {
-
             // Ignore if path data is not available.
             if (pathArray === undefined || pathArray.length === 0) {
                 return;
             }
 
-            console.log('@showpath called: number of path = ' + pathArray.length);
-            // Add edges from DAG (1st neighbours only)
+            // Map of child nodes
+            var parent = pathArray[0][1];
+            var children = {};
+
+            // Add edges from DAG in the database (1st neighbours only)
             var edgeExists = this.addHiddenEdges(pathArray);
 
             // Valid path - all nodes exists on the current view.
             var validPaths = [];
-            var targetNodes = {};
+            var sourceNodes = {};
             _.each(pathArray, function (path) {
-                targetNodes[path[1]] = true;
+                sourceNodes[path[0]] = true;
                 // neighbours are always valid.
                 var isValidPath = true;
                 var pathLength = path.length;
+                if(pathLength === 2 && path[0] !== parent) {
+                    // This is a child.
+                    children[path[0]] = true;
+                }
+
                 for (var i = 0; i < pathLength - 1; i++) {
                     var edge = edgeExists[[path[i] + '-' + path[i + 1]]];
                     if (edge === undefined) {
@@ -316,21 +334,92 @@ define([
                 }
             });
 
-
             _.each(validPaths, function (path) {
                 _.each(path, function (pathNode) {
-                    targetNodes[pathNode] = true;
+                    sourceNodes[pathNode] = true;
                 });
             });
 
-            console.log(targetNodes);
-            this.highlight(targetNodes, false, pathArray[0][0]);
+            var queryNode = pathArray[0][0];
+            this.highlight(sourceNodes, false, queryNode, children);
+        },
+
+
+        /**
+         * Highlight selected path/nodes
+         *
+         * @param sourceNodes
+         * @param nodesOnly
+         * @param queryNode
+         */
+        highlight: function (sourceNodes, nodesOnly, queryNode, children) {
+            if (nodesOnly === false) {
+                SigmaRenderer.iterEdges(function (edge) {
+                    if (edge.color !== SELECTED_NODE_COLOR && edge.color !== DIM_COLOR) {
+                        // Save original color to restore later.
+                        edge.attr.originalColor = edge.color;
+                    }
+
+                    var sourceId = edge.source;
+                    var targetId = edge.target;
+
+                    if (sourceNodes[sourceId] === undefined || sourceNodes[targetId] === undefined) {
+                        // Not on the path.  DIM all of those.
+                        if (!edge.attr.grey) {
+                            edge.color = DIM_COLOR;
+                            edge.attr.grey = true;
+                        }
+                    } else if (edge.label === EXTRA_EDGE) {
+                        edge.color = EXTRA_EDGE_COLOR;
+                        edge.attr.grey = false;
+                    } else if (children[sourceId] && sourceId !== queryNode && targetId === queryNode) {
+                        edge.color = CHILD_NODE_COLOR;
+                        edge.attr.grey = false;
+                    } else if (!(children[sourceId] && children[targetId])) {
+                        edge.color = SELECTED_NODE_COLOR;
+                        edge.attr.grey = false;
+                    }
+                });
+            } else {
+                SigmaRenderer.iterEdges(function (edge) {
+                    if (edge.color !== SELECTED_NODE_COLOR && edge.color !== DIM_COLOR) {
+                        edge.attr.originalColor = edge.color;
+                    }
+                    edge.color = DIM_COLOR;
+                    edge.attr.grey = true;
+                });
+            }
+
+            // Update node color
+            SigmaRenderer.iterNodes(function (node) {
+                if (node.color !== CHILD_NODE_COLOR && node.color !== SELECTED_NODE_COLOR && node.color !== DIM_COLOR && node.color !== QUERY_NODE_COLOR) {
+                    node.attr.originalColor = node.color;
+                }
+
+                if (queryNode !== undefined && node.id === queryNode) {
+                    node.color = QUERY_NODE_COLOR;
+                    node.attr.grey = false;
+                    node.forceLabel = true;
+                } else if (children[node.id]) {
+                    node.color = CHILD_NODE_COLOR;
+                    node.attr.grey = false;
+                    node.forceLabel =true;
+                } else if (!sourceNodes[node.id]) {
+                    node.color = DIM_COLOR;
+                    node.attr.grey = true;
+                    node.forceLabel = false;
+                } else {
+                    node.color = SELECTED_NODE_COLOR;
+                    node.attr.grey = false;
+                    node.forceLabel = true;
+                }
+            }).draw(2, 2, 2);
         },
 
         refresh: function () {
             SigmaRenderer
                 .iterEdges(function (edge) {
-                    if (edge.label === 'extra') {
+                    if (edge.label === EXTRA_EDGE) {
                         SigmaRenderer.dropEdge(edge.id);
                     }
                     edge.color = edge.attr.originalColor;
@@ -346,64 +435,8 @@ define([
 
         fit: function () {
             SigmaRenderer.position(0, 0, 1).draw();
-        },
-
-        highlight: function (targetNodes, nodesOnly, queryNode) {
-
-            if (nodesOnly === false) {
-                SigmaRenderer.iterEdges(function (edge) {
-                    if (edge.color !== SELECTED_NODE_COLOR && edge.color !== DIM_COLOR) {
-                        edge.attr.originalColor = edge.color;
-                    }
-                    var sourceId = edge.source;
-                    var targetId = edge.target;
-
-                    if (targetNodes[sourceId] === undefined || targetNodes[targetId] === undefined) {
-                        // Not on the path.  DIM all of those.
-                        if (!edge.attr.grey) {
-                            edge.color = DIM_COLOR;
-                            edge.attr.grey = true;
-                        }
-                    } else if (edge.label === 'extra') {
-                        edge.color = 'rgba(255,94,25,0.7)';
-                        edge.attr.grey = false;
-                    } else {
-                        edge.color = SELECTED_NODE_COLOR;
-                        edge.attr.grey = false;
-                    }
-                });
-            } else {
-                SigmaRenderer.iterEdges(function (edge) {
-                    if (edge.color !== SELECTED_NODE_COLOR && edge.color !== DIM_COLOR) {
-                        edge.attr.originalColor = edge.color;
-                    }
-
-                    edge.color = DIM_COLOR;
-                    edge.attr.grey = true;
-
-                });
-            }
-
-            SigmaRenderer.iterNodes(function (node) {
-                if (node.color !== SELECTED_NODE_COLOR && node.color !== DIM_COLOR && node.color !== QUERY_NODE_COLOR) {
-                    node.attr.originalColor = node.color;
-                }
-
-                if (queryNode !== undefined && node.id === queryNode) {
-                    node.color = QUERY_NODE_COLOR;
-                    node.attr.grey = false;
-                    node.forceLabel = true;
-                } else if (!targetNodes[node.id]) {
-                    node.color = DIM_COLOR;
-                    node.attr.grey = true;
-                    node.forceLabel = false;
-                } else {
-                    node.color = SELECTED_NODE_COLOR;
-                    node.attr.grey = false;
-                    node.forceLabel = true;
-                }
-            }).draw(2, 2, 2);
         }
+
     });
 
     return NetworkView;
